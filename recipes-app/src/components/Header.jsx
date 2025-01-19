a1cc862404e6f46ac00ecee141c57109a0f4006c
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-01-19 13:22:25 +0100

    App

---
+++ recipes-app/src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <span className="headerItems">
+          <img
+            className="icon"
+            src="./imgs/chefIcon.png
+"
+            alt="chef icon"
+          />
+          <span>
+            <h1>Your recipes</h1>
+          </span>
+        </span>
+      </nav>
+    </header>
+  );
+}
